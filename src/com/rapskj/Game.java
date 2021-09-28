006bb2249f3ee0a34a4b2575a272a04d7134acf8
Josef Praks <pepapraks@seznam.cz>
2021-09-28 17:52:52 +0200

    2nd Test

--- src/com/rapskj/Game.java
+++ src/com/rapskj/Game.java
@@ -1,10 +1,15 @@
 package com.rapskj;
-import javax.swing.JFrame; //Import JFrame pro generovani GUI
+import javax.swing.*;
 import java.awt.*;
 
 public class Game {
 
     JFrame okno;
+    JPanel titlePanel, startPanel;
+    JLabel titleLabel;
+    Font titleFont = new Font("Calibri", Font.PLAIN, 70);
+    JButton startButton;
+    Font normalText = new Font("Calibri", Font.PLAIN,40);
 
     public static void main(String[] args) {
 
@@ -18,7 +23,44 @@ public class Game {
         okno.setSize(800,600);
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Uzivatel muze zavrit okno
         okno.getContentPane().setBackground(Color.BLACK); //Nastaveni pozadi
-        okno.setLayout(null);
+        okno.setLayout(null); //Zakazani defaultniho layoutu
+        okno.setLocationRelativeTo(null); //okno se vzdy otevre uprostred obrazovky
+
+
+        titlePanel = new JPanel(); //Inicializace JPanel
+        titlePanel.setBounds(100,100,600,150);
+        /*
+        Generace panelu ve kterem bude popis hry.
+        x: osa x
+        y: osa y
+        width: sirka
+        height: vyska
+        */
+        titlePanel.setBackground(Color.BLACK);
+        titleLabel = new JLabel("Sorrow Euphoria"); //Inicializace JLabel
+        titleLabel.setForeground(Color.white);
+        titleLabel.setFont(titleFont);
+        titlePanel.add(titleLabel); //Pridam Label do Panelu
+
+        startPanel = new JPanel();
+        startPanel.setBounds(300,400,200,100);
+        startPanel.setBackground(Color.BLACK);
+
+
+        startButton = new JButton("Start");
+        startButton.setBackground(Color.BLACK);
+        startButton.setForeground(Color.WHITE);
+        startButton.setBorder(null);
+        startButton.setFont(normalText);
+        startPanel.add(startButton);
+
+        okno.add(titlePanel);
+        okno.add(startPanel);
+
+
+
+
+        okno.setVisible(true); //Zobrazeni okna
 
 
     }
